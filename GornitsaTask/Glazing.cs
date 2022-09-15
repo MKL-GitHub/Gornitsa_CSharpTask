1633f874b567f5b637d5e774c5348b25d7421419
Mikhail Tkachev <tritium.mkl@gmail.com>
2022-09-15 19:31:46 +0300

    Push project.

---
+++ GornitsaTask/Glazing.cs
@@ -0,0 +1,76 @@
+﻿using System.Text;
+
+namespace GornitsaTask;
+
+/// <summary>
+/// Реализует парсинг артикула стеклопакета.
+/// </summary>
+internal class Glazing
+{
+    private int _chamberness = 0;
+    private int _glazingThickness = 0;
+    private int _glassThickness = 0;
+
+    public Glazing(string vendorCode) => Parse(vendorCode);
+
+    /// <summary>
+    /// Возвращает камерность стеклопакета.
+    /// </summary>
+    public int Chamberness => _chamberness;
+
+    /// <summary>
+    /// Возвращает толщину стеклопакета.
+    /// </summary>
+    public int GlazingThickness => _glazingThickness;
+
+    /// <summary>
+    /// Возвращает толщину стекла.
+    /// </summary>
+    public int GlassThickness => _glassThickness;
+
+    private void Parse(string vendorCode)
+    {
+        List<int> numbers = new();
+        StringBuilder sb = new();
+        bool state = true;
+
+        for (int i = 0; i < vendorCode.Length; i++)
+        {
+            if (char.IsDigit(vendorCode[i]) && state)
+            {
+                sb.Append(vendorCode[i]);
+
+                if (i < vendorCode.Length - 1) continue;
+            }
+
+            if (sb.Length > 0)
+            {
+                numbers.Add(int.Parse(sb.ToString()));
+                sb.Clear();
+                state = false;
+            }
+
+            if (vendorCode[i] == '\\' || vendorCode[i] == '/')
+            {
+                state = true;
+            }
+        }
+
+        foreach (int number in numbers)
+        {
+            if (!state)
+            {
+                _glassThickness += number;
+                state = true;
+            }
+            else
+            {
+                _glazingThickness += number;
+                state = false;
+            }
+        }
+
+        _glazingThickness += GlassThickness;
+        _chamberness = numbers.Count == 3 ? 1 : 2;
+    }
+}
---
+++ GornitsaTask/Program.cs
@@ -0,0 +1,19 @@
+﻿using GornitsaTask;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Glazing glazing = new("6 SG HD Silver Grey 32 ЗАК/16/6 м1/16/6 ЗАК");
+
+        Print(glazing);
+    }
+
+    private static void Print(Glazing glazing)
+    {
+        Console.WriteLine(
+            $"Камерность: {glazing.Chamberness}\n" +
+            $"Толщина СП: {glazing.GlazingThickness}\n" +
+            $"Толщина стекла: {glazing.GlassThickness}");
+    }
+}
